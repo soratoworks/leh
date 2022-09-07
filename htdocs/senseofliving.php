7cff15c1e92049fd9221944822aff20f4ea3ceef
sorato <sorato.takagaki@gmail.com>
2022-09-08 00:51:07 +0900

    senseoflivingをDL

---
+++ htdocs/senseofliving.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja" dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>LEH</title>
+<meta http-equiv="content-style-type" content="text/css" />
+<meta http-equiv="content-script-type" content="text/javascript" />
+<meta name="robots" content="index,follow" />
+<meta name="keywords" content="LEH" />
+<meta name="description" content=" The official website of Leh" />
+<meta name="robots" content="index,follow" />
+<meta name="keywords" content="LEH" />
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<link href="css/2018ss_menu.css" rel="stylesheet" type="text/css" media="all" />
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/css_white.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/css_white_2018aw.css">
+   <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+   <link href="css/bootstrap.css" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet">
+   <script src="http://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+   <link href="../css/css_white_2018aw.css" rel="stylesheet" type="text/css" media="all" />
+   <link href="../css/menu.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" type="text/css" href="lib/css/import.css">
+
+
+    
+    <!-- ファビコン -->
+<link rel="icon" href="common/img/favicon.ico">
+ 
+<!-- スマホ用アイコン -->
+<link rel="apple-touch-icon" sizes="180x180" href="common/img/apple-touch-icon.png">
+
+
+</head>
+
+<body>
+<div id="container">
+<div id="wrapper_memo">
+
+<!-- 左側メニューここから -->
+
+		<?php
+			include "left_menu.php";
+		?>
+
+		<?php
+			include "menu_header.php";
+		?>
+
+
+<!-- /left_space -->
+	<!-- 右側コンテンツここから -->	
+
+  <style>
+    .img-mb-20{
+      margin-bottom:50px;
+    }
+
+    /* モバイルで消えるようにする　*/
+@media (max-width: 1023px){
+
+  .slider img{
+    width:100%;
+  }
+}
+</style>
+<div id="right_space">
+    <h1>SENSE OF LIVING</h1>
+
+<div class="slider">
+      <div><img src="lib/img/senseofliving/202101/001.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/002.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/003.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/004.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/005.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/006.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/202101/007.jpg" class="img-mb-20" alt="" /></div>
+	
+     <div><img src="lib/img/senseofliving/201708_senseofliving01.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving02.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving03.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving04.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving05.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving06.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving07.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving08.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving09.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving10.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving11.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving12.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving13.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving14.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving15.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving16.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving17.jpg" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving18.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving19.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving20.jpg" class="img-mb-20" alt="" /></div>
+     <div><img src="lib/img/senseofliving/201708_senseofliving21.jpg" alt="" /></div>
+
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_132.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_34.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_111.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_52.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_79.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_78.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_96.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_29.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_126.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_90.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_88.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_115.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_98.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_120.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/2017INDIA_AFRICA_91.jpg" class="img-mb-20" alt="" /></div>
+
+      <div><img src="lib/img/senseofliving/sol_23.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_07.jpg" alt="" /></div>      
+      <div><img src="lib/img/senseofliving/sol_49.jpg" alt="" /></div>  
+      <div><img src="lib/img/senseofliving/sol_48.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_47.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_02.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_02.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_15.jpg" class="img-mb-20" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_14.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_09.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_45.jpg" alt="" /></div> 
+      <div><img src="lib/img/senseofliving/sol_12.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_16.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_06.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_01.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_16.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_11.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_22.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/201407/sol_17.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_13.jpg" alt="" /></div>
+      <div><img src="lib/img/senseofliving/sol_06.jpg" alt="" /></div>
+</div>
+
+</div>
+<br clear="all">
+<div class="js-pageTop"></div>
+
+<!--FOOTERここから-->
+
+    <div id="footer">
+        Copyright (C) All Rights Reserved by Leh.<br />
+        <span class="company_name"><a href="../../index.html">www.leh.jp</a></span><br /><br />
+		<a href="mailto:info@leh.jp"><img src="../../img/mail.jpg" alt="LEHにメール" width="18" height="12" border="0" /></a>
+    </div>
+<!--FOOTERここまで-->
+</div>
+</div>
+<!--全体の囲みここまで-->
+
+<!--   解析用  -->
+		<?php
+			include "analyze.php";
+		?>
+<!--   解析用  -->
+<script type="text/javascript" src="js/menu.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Modaal/0.4.4/js/modaal.min.js"></script>
+<!--自作のJS-->
+<script type="text/javascript" src="js/modaal.js"></script>
+</body>
+</html>
