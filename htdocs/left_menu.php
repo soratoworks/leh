90f0ee6283fef89481e4f40963ed2a12dba63c6b
sorato <sorato.takagaki@gmail.com>
2024-07-20 23:52:41 +0900

    初回プレビュー時 htaccessの飛び先は本番化する時は変更する必要がある

---
+++ htdocs/left_menu.php
@@ -0,0 +1,16 @@
+    <div id="gnav-top">
+        <ul>
+        <li><a href="index.html">TOP</a></li>
+        <li><a href="collection.php">COLLECTION</a></li>
+        <li><a href="memo.php">JOURNAL&INFO</a></li>
+        <li><a href="silence.php">SILENCE</a></li>
+        <li><a href="stockist.php">STOCKIST</a></li>
+        <li><a href="senseofliving.php">SENSE OF LIVING</a></li>
+        <li><a href="aboutus_en.php">ABOUT US</a></li>
+        <li><a href="movie.php">MOVIE</a></li>
+        <li><a href="http://blog.leh.jp/" target="_blank">NOTE</a></li>
+        <li><a href="https://instagram.com/leh__official/" target="_blank"><img src="lib/img/com/leh_instagram.png" alt="INSTAGRAM" /></a></li>
+       </ul>
+    </div>
+
+   
--- htdocs/menu_header.php
+++ htdocs/menu_header.php
@@ -4,18 +4,18 @@
       
               <nav>
                 <div class="inner">
-                  <ul>
-                    <li><a href="index.html"><img src="lib/img/com/gnav_top.png" alt="TOP" /></a></li>
-                    <li><a href="collection.php"><img src="lib/img/com/gnav_collection.png" alt="COLLECTION" /></a></li>
-                    <li><a href="memo.php"><img src="lib/img/com/gnav_Journal.png" alt="JOURNAL&INFO" /></a></li>
-                    <li><a href="stockist.php"><img src="lib/img/com/gnav_stockist.png" alt="STOCKIST" /></a></li>
-                    <li><a href="senseofliving.php"><img src="lib/img/com/gnav_sol.png" alt="SENSEOFLIVING" /></a></li>
-                    <li><a href="aboutus_en.php"><img src="lib/img/com/gnav_about.png" alt="ABOUTUS" /></a></li>
-                    <li><a href="movie.php"><img src="lib/img/com/gnav_movie.png" alt="MOVIE" /></a></li>
-                    <li><a href="http://blog.leh.jp/" target="_blank"><img src="lib/img/com/gnav_note.png" alt="MEMO" /></a></li>
-                    <li><a href="https://leh.handcrafted.jp/" target="_blank"><img src="lib/img/com/gnav_onlinestore.png" alt="ONLINESTORE" /></a></li><br>
-                    <li><a href="https://instagram.com/leh__official/" target="_blank"><img src="lib/img/com/leh_instagram.png" alt="INSTAGRAM" /></a></li>
-                  </ul>
+                <ul>
+        <li><a href="index.html">TOP</a></li>
+        <li><a href="collection.php">COLLECTION</a></li>
+        <li><a href="memo.php">JOURNAL&INFO</a></li>
+        <li><a href="silence.php">SILENCE</a></li>
+        <li><a href="stockist.php">STOCKIST</a></li>
+        <li><a href="senseofliving.php">SENSE OF LIVING</a></li>
+        <li><a href="aboutus_en.php">ABOUT US</a></li>
+        <li><a href="movie.php">MOVIE</a></li>
+        <li><a href="http://blog.leh.jp/" target="_blank">NOTE</a></li>
+        <li><a href="https://instagram.com/leh__official/" target="_blank"><img src="lib/img/com/leh_instagram.png" alt="INSTAGRAM" /></a></li>
+       </ul>
                 </div>
               </nav>
             
